1c8ca13d3ef3dae36c007567648d2df8094f72f5
Harishankar <94908935+MrKolaruGaming@users.noreply.github.com>
2023-10-04 17:28:23 +0530

    if else

---
+++ index.js
@@ -0,0 +1,59 @@
+var message;
+message = 'Hello, World!';
+console.log(message);
+console.log(typeof message);
+var age = 23;
+console.log(typeof age);
+var wired;
+console.log(typeof wired);
+var Kull = null;
+console.log(typeof Kull);
+let h = true;
+console.log(typeof h);
+
+let result = 1;
+console.log(result);
+result = ++result;
+console.log(result);
+result = ++result +5;
+console.log(result);
+result = result+20;
+console.log(result);
+result = result +10;
+console.log(result);
+
+let lang ="javascript";
+console.log("hello " + lang + " 1001 ");
+
+let a = "red";
+let b = "blue";
+c=a;
+a=b;
+b=c;
+console.log(a);
+console.log(b); 
+
+// if - else 
+
+let hour = 25;
+
+if ( hour>= 6 && hour <12  ) {
+    console.log('Good Morning');
+}
+else if (hour>=12  && hour <18 ) {
+    console.log ('good afternoon') ;
+}
+else if (hour>= 18 && hour<20){
+    console.log ('good evening')
+}
+else if (hour>= 20 && hour<24){
+    console.log ('good night')
+}
+else {
+    console.log('dont make a joke')
+}
+
+
+
+
+  
